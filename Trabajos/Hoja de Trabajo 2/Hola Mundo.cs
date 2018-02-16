b6f8eaffd24fee61bc4fcb2a91bb0754e122cd2a
Pajoponce <poncebarrera@icloud.com>
2018-02-16 17:07:18 -0600

    Trabajos

---
+++ Trabajos/Hoja de Trabajo 2/Hola Mundo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hoja_de_Trabajo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hermanos.X=0;
+        }
+    }
+}
---
+++ Trabajos/Hoja de Trabajo 2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hoja_de_Trabajo_2
+{
+    public class Hermanos
+    {
+        int x;
+    }
+
+}
