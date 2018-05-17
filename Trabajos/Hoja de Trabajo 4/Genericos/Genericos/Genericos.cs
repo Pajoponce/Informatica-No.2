66a72b8cf753ea65501c41e669074329f339ae62
Pablo Ponce <poncebarrera@icloud.com>
2018-05-16 18:00:02 -0600

    Hoja_de_Trabajo_4

---
+++ Trabajos/Hoja de Trabajo 4/Genericos/Genericos/Genericos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    public class Generico
+    {
+        public static T Head<T>(T[] Arr)
+        {
+            return Arr[0];
+        }
+        public static T Tails<T>(T[] Arr)
+        {
+            return Arr[0];
+        }
+    }
+}
---
+++ Trabajos/Hoja de Trabajo 4/Genericos/GenericosTests/GenericosTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Genericos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos.Tests
+{
+    [TestClass()]
+    public class GenericosTests
+    {
+        [TestMethod()]
+        public void HeadTest<T>()
+        {
+        }
+    }
+}
